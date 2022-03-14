646e28d49309813ce0f12356d8ebcd6760e8b773
AttaullahSaeed <attaullahsaeed77@gmail.com>
2022-03-14 23:06:02 +0500

    Updates

---
+++ precache-manifest.dd05c71515a4ac676b4d06b163d18945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe0ee5ec7eaed0a7c56a7ab768cfef4",
+    "url": "/mobile_call/index.html"
+  },
+  {
+    "revision": "93e8e46c80c4b41b0648",
+    "url": "/mobile_call/static/css/2.42fe6979.chunk.css"
+  },
+  {
+    "revision": "1458f63d1ccf687ccaad",
+    "url": "/mobile_call/static/css/main.22730559.chunk.css"
+  },
+  {
+    "revision": "93e8e46c80c4b41b0648",
+    "url": "/mobile_call/static/js/2.0e2e6755.chunk.js"
+  },
+  {
+    "revision": "5ac4e3299dd902736e16c8e7a127c843",
+    "url": "/mobile_call/static/js/2.0e2e6755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1458f63d1ccf687ccaad",
+    "url": "/mobile_call/static/js/main.5cc38f45.chunk.js"
+  },
+  {
+    "revision": "b709af79401d739b4abb",
+    "url": "/mobile_call/static/js/runtime-main.bd7a7f03.js"
+  }
+]);
