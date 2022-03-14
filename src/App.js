0bcd7fc1ab57e5f54560a82718f7577e7b983836
AttaullahSaeed <attaullahsaeed77@gmail.com>
2022-03-14 22:03:37 +0500

    atta

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 
 import Home from "./Home";
 
-import Table from "./Table";
-
 const App = () => {
   return (
     <div>
       <Home />
-      {/* <Table /> */}
     </div>
   );
 };
